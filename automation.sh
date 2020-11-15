85f92852d058988fd41f32bf904d070caf5c8447
parvathysajee <parvathysajee95@gmail.com>
2020-11-15 21:18:51 +0000

    latest

--- automation.sh
+++ automation.sh
@@ -6,5 +6,5 @@ activate () {
 activate
 rm -rf ScProject
 git clone https://github.com/parvathysajee/ScProject.git
-cd ScProject/sample-code 
-python classify.py  --model-name test.h5 --captcha-dir input_data --output stuff.txt --symbols symbols.txt
+cd ScProject
+python classify.py  --model-name test --captcha-dir check_data --output stuff.txt --symbols symbols.txt
