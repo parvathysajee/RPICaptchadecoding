e00d0b97bc569820d852b6e50e045f437742c61b
parvathysajee <parvathysajee95@gmail.com>
2020-11-15 20:28:46 +0000

    latest

--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-
 import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -191,6 +190,12 @@ def main():
         except KeyboardInterrupt:
             print('KeyboardInterrupt caught, saving current weights as ' + args.output_model_name+'_resume.h5')
             model.save_weights(args.output_model_name+'_resume.h5')
+            
+    with tf.device('/device:GPU:0'):
+        model = tf.keras.models.load_model('test.h5.h5')
+        converter = tf.lite.TFLiteConverter.from_keras_model(model)
+        tflite_model = converter.convert()
+        open(args.output_model_name+".tflite", "wb").write(tflite_model)
 
 if __name__ == '__main__':
     main()
